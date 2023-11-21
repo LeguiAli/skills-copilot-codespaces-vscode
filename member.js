449e81341f45e6ad41648f489de6bea83b653c9b
LeguiAli <115218403+LeguiAli@users.noreply.github.com>
2023-11-21 03:31:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillMembers() {
+    var members = ['egoing', 'k8805', 'hoya'];
+    for (var i = 0; i < members.length; i++) {
+        console.log('array loop', members[i]);
+    }
+    return members;
+}
